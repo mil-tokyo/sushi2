f349e8e69b2b2db195ef17bbf3f3d82c9fd60131
Hidaka Masatoshi <hidaka@mi.t.u-tokyo.ac.jp>
2016-06-19 18:00:47 +0900

    browserify without webcl

--- do_browserify.sh
+++ do_browserify.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-browserify index.js -o browser/sushi.js -s Sushi --external 'src/cl/handwrittenjs/driver_opencl.js'
+browserify index.js -o browser/sushi_cl.js -s Sushi --external 'src/cl/handwrittenjs/driver_opencl.js'
+browserify index.js -o browser/sushi.js -s Sushi --external 'src/cl/handwrittenjs/sushi_cl.js'
