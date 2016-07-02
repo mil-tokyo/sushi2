33026267d38251841d6e3340c2fa24d0b8f5b2ed
Masatoshi Hidaka <hidaka@mi.t.u-tokyo.ac.jp>
2016-07-02 16:47:30 +0900

    change browser javascript name to milsushi2 to match npm

--- do_browserify.sh
+++ do_browserify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-browserify index.js -o browser/sushi_cl.js -s Sushi --external 'src/cl/handwrittenjs/driver_opencl.js'
-browserify index.js -o browser/sushi.js -s Sushi --external 'src/cl/handwrittenjs/sushi_cl.js'
+browserify index.js -o browser/milsushi2_cl.js -s milsushi2 --external 'src/cl/handwrittenjs/driver_opencl.js'
+browserify index.js -o browser/milsushi2.js -s milsushi2 --external 'src/cl/handwrittenjs/sushi_cl.js'
