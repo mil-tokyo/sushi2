a62dac8beba49d42b4ce0e85725a9207d304fc60
Hidaka Masatoshi <hidaka@mi.t.u-tokyo.ac.jp>
2016-11-03 15:09:23 +0900

    add license header for browserify

--- do_browserify.sh
+++ do_browserify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-browserify index.js -o browser/milsushi2_cl.js -s milsushi2 --external 'src/cl/handwrittenjs/driver_opencl.js'
-browserify index.js -o browser/milsushi2.js -s milsushi2 --external 'src/cl/handwrittenjs/sushi_cl.js'
+browserify index.js -o browser/milsushi2_cl.js -s milsushi2 -p \[ browserify-header --file browser/header.js \] --external 'src/cl/handwrittenjs/driver_opencl.js'
+browserify index.js -o browser/milsushi2.js -s milsushi2 -p \[ browserify-header --file browser/header.js \] --external 'src/cl/handwrittenjs/sushi_cl.js'
