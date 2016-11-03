8afd8395dad7d252d9dae87b4ce608190075b3fa
Hidaka Masatoshi <hidaka@mi.t.u-tokyo.ac.jp>
2016-11-03 15:09:15 +0900

    add license header for browserify

---
+++ browser/header.js
@@ -0,0 +1,4 @@
+/*!
+ Sushi2 library (c) 2016 Machine Intelligence Laboratory (The University of Tokyo), MIT License.
+ clBLAS library Copyright 2013 Advanced Micro Devices, Inc., Apache License Version 2.0.
+*/
